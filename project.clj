18a2eb3fdf756f613cd84e263b41b6366e3b64c4
Daniel Smith <jellymann@gmail.com>
2014-04-15 14:58:15 +0200

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject lsystem "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "L-System"
+  :url "http://github.com/jellymann/lsystem-clojure"
+  :license {:name "MIT License"
+            :url "opensource.org/licenses/MIT"
   :dependencies [[org.clojure/clojure "1.5.1"] [quil "1.7.0"]]
   :main lsystem.visual)
