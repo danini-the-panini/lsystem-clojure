4643c9c3976962a12142207f144795d704c79bef
Daniel Smith <jellymann@gmail.com>
2014-04-15 15:12:42 +0200

    Fixed error in project.clj

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "L-System"
   :url "http://github.com/jellymann/lsystem-clojure"
   :license {:name "MIT License"
-            :url "opensource.org/licenses/MIT"
+            :url "opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"] [quil "1.7.0"]]
   :main lsystem.visual)
